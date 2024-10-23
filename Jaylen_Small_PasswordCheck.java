f32aeecce347cadbf7b3291cf86b04078b19460f
Pazmaz <162527327+Pazmazz@users.noreply.github.com>
2024-10-23 15:08:57 -0400

    Add files via upload

--- Jaylen_Small_PasswordCheck.java
+++ Jaylen_Small_PasswordCheck.java
@@ -1,4 +1,56 @@
+/*Author: Jaylen Small 
+Written: 10/23/24
+
+Solution to the password check assignemnt on blackboard
+*/
+
 public class Jaylen_Small_PasswordCheck {
+    // isValidPassword method that checks to see if a password is valid
+    public static boolean isValidPassword(String string){
+        // A validation counter that needs to stay 0 in order the password to be valid
+        int validCounter = 0;
+        
+        // Initializes digitCount and stringChar variables
+        int digitCount = 0;
+        char stringChar;
+
+        // For loop that checks each character in the string
+        for (int i = 0; i < string.length(); i++){
+            stringChar = string.charAt(i);
+            boolean hasDigit = Character.isDigit(stringChar);
+            boolean hasLetter = Character.isLetter(stringChar);
+            
+            // If the string length is greater than 8(7 java charcters) it substracts from the validation counter and breaks out the loop
+            if (i > 7){
+                validCounter--;
+                break;
+            } 
+            
+            // If the character is neither a number or character, it substracts from the validation counter and breaks out the loop
+            if (!(hasDigit || hasLetter)){
+                validCounter--;
+                break;
+            }
+
+            // Counts how many digits are in the string, if it's more than 2 it substracts from the validation counter and breaks out the loop
+            if (hasDigit == true){
+                digitCount++;
+            }
+
+            if (digitCount > 2){
+                validCounter--;
+                break;
+            }
+        }
+
+        // If the validation counter stays 0, the password is valid, otherwise it's invalid
+        if(validCounter < 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
     public static void main(String[] args) {
         // Prompt the user to enter a password
         java.util.Scanner input = new java.util.Scanner(System.in);
@@ -10,45 +62,6 @@ public class Jaylen_Small_PasswordCheck {
         }
         else {
           System.out.println("Invalid Password");
-        }
-      }//end main method
-    
-    public static boolean isValidPassword(String string){
-        int validPassword = 0;
-        int digitCount = 0;
-        char stringChar;
-
-        for (int i = 0; i < string.length(); i++){
-            stringChar = string.charAt(i);
-            boolean hasDigit = Character.isDigit(stringChar);
-            boolean hasLetter = Character.isLetter(stringChar);
-            
-            if (i > 7){
-                validPassword--;
-            } 
-            
-            if (!(hasDigit || hasLetter)){
-                validPassword--;
-            }
-        }
-
-        for (int i = 0; i < string.length(); i++){
-            stringChar = string.charAt(i);
-            boolean hasDigit = Character.isDigit(stringChar);
-            
-            if (hasDigit == true){
-                digitCount++;
-            }
-
-            if (digitCount > 2){
-                validPassword--;
-            }
-        }
-
-        if(validPassword < 0){
-            return false;
-        } else {
-            return true;
         }
     }
 }
